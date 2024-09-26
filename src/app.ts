094dd4b7996a7af3bbc6f43676ab9232c662b078
Tonysw2 <anthonyrmataa@gmail.com>
2024-09-26 14:01:08 -0300

    refactor: remove unnecessary log

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import { faker } from '@faker-js/faker'
 import fastifyCookie from '@fastify/cookie'
 import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
@@ -39,21 +38,3 @@ app.setErrorHandler((error, _, reply) => {
 
   reply.code(500).send({ error: 'Internal Server Error' })
 })
-
-console.log(
-  JSON.stringify({
-    name: faker.company.name(),
-    authorName: faker.person.fullName(),
-    email: faker.internet.email(),
-    passwordHash: faker.internet.password(),
-    whatsapp: faker.phone.number(),
-    zipCode: faker.location.zipCode(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    neighborhood: faker.location.streetAddress(),
-    street: faker.location.street(),
-
-    latitude: faker.location.latitude(),
-    longitude: faker.location.longitude(),
-  }),
-)
